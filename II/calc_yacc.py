617990c23ad725db50ec6e81d9c4c07c13439a07
nafe kzir <nafe93@windowslive.com>
2019-10-10 22:06:21 +0300

    finish second hw2

--- II/calc_yacc.py
+++ II/calc_yacc.py
@@ -18,6 +18,43 @@ def parsing_string_for_parsing_string(my_string):
     return my_string
 
 
+# Проверка на статисфайбл
+def is_satisfiable(debug=0):
+    # First tuple
+    for index_tuple, _tuple in enumerate(global_list):
+        for index_tuple_list, _tuple_list in enumerate(_tuple):
+            if _tuple_list is not None and '~' in _tuple_list:
+                # Second tuple
+                for index_second_tuple, second_tuple in enumerate(global_list):
+                    for index_second_tuple_list, second_tuple_list in enumerate(second_tuple):
+                        # compare
+                        if second_tuple_list is not None and _tuple_list is not None:
+                            if _tuple_list == '~' + second_tuple_list:
+                                _tuple_list = None
+                                second_tuple_list = None
+                                _tuple[index_tuple_list] = None
+                                second_tuple[index_second_tuple_list] = None
+                                if debug == 1:
+                                    print(global_list)
+
+
+# Проверить что все элементы None
+def all_is_none():
+    counter_list = len(global_list) * 2
+    counter = 0
+    for index_tuple, _tuple in enumerate(global_list):
+        for index_tuple_list, _tuple_list in enumerate(_tuple):
+            if _tuple_list == None:
+                counter += 1
+
+    if counter_list == counter:
+        print("not satisfiable")
+    else:
+        print("satisfiable")
+
+    global_list.clear()
+
+
 # parser logic
 
 # Переменные
@@ -54,7 +91,6 @@ def p_and_exp_exp_operators(p):
         global_list.append([p[3], None])
 
 
-
 def p_and_exp_factor_operators(p):
     '''expression  : expression AND factor'''
     p[0] = p[1] + '/\\' + p[3]
@@ -72,7 +108,6 @@ def p_and_factor_exp_operators(p):
 def p_and_factor_factor_operators(p):
     '''expression  : factor AND factor'''
     p[0] = p[1] + '/\\' + p[3]
-
 
 
 # Операции над двойным отрицанием
@@ -97,20 +132,8 @@ while True:
         break
     if not s: continue
     result = parser.parse(s)
-    # print(result)
-    print(global_list)
-
-    for index_tuple, _tuple in enumerate(global_list):
-        # First tuple
-        for index_tuple_list, _tuple_list in enumerate(_tuple):
-            if _tuple_list is not None and '~' in _tuple_list:
-                # Second tuple
-                for index_second_tuple, second_tuple in enumerate(global_list):
-                    for index_second_tuple_list, second_tuple_list in enumerate(second_tuple):
-                        if second_tuple_list is not None and _tuple_list is not None:
-                            if _tuple_list == '~' + second_tuple_list:
-                                _tuple_list = None
-                                second_tuple_list = None
-                                _tuple[index_tuple_list] = None
-                                second_tuple[index_second_tuple_list] = None
-                                print(global_list)
+    # debug = 1 or 0
+    # 1 view how the logic work
+    # 0 disable how the logic work
+    is_satisfiable(debug=0)
+    all_is_none()
